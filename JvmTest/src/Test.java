0f37ea0efe4cd01e352e62d08ad09928e9f563b4
MuXiaoFeng <1501107319@qq.com>
2019-03-29 14:40:38 +0800

    jvm调优相关

---
+++ JvmTest/src/Test.java
@@ -0,0 +1,25 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		/*String s1 = "a";
+
+		String s2 = s1 + "b";
+
+		String s3 = "a" + "b";
+		
+		System.out.println(s2 == "ab");
+
+		System.out.println(s3 == "ab");*/
+		
+		String a ="a";
+		String b ="b";
+		String c=a+b;
+		System.out.println(c =="ab");
+		String d="a"+"b";
+		System.out.println(d =="ab");
+
+
+	}
+
+}
